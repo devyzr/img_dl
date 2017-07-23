0e41e396b43cee2be4ff8f3c77dfc6a5fd002d5f
Daniel Verdin <devyzr@users.noreply.github.com>
2017-07-23 11:56:14 -0500

    Updated to be more generic
    Also gets the fillename from the link provided

--- image_dl.py
+++ image_dl.py
@@ -1,10 +1,10 @@
 import requests
 import shutil
 
-settings = 'https://www.google.com.mx/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
-
-r = requests.get(settings, stream=True)
-if r.status_code == 200:
-    with open('image.png', 'wb') as f:
-        r.raw.decode_content = True
-        shutil.copyfileobj(r.raw, f)  
+def save_image(link):
+    filename = link.split('/')[1:]
+    r = requests.get(link, stream=True)
+    if r.status_code == 200:
+        with open(filename, 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)  
